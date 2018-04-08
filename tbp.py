56bdf1fe7f100422887654c658b562bf647670b9
Sean Camden <sean@seancamden.com>
2018-04-07 18:25:42 -0700

    Added functions for printing on part of speach and first/last letters.

--- tbp.py
+++ tbp.py
@@ -10,3 +10,23 @@ def jj(l="null"):
             if key[0] == "J":
                 print key,value
 
+def p(part,l="null"):
+    if type(l) == int:
+        for value,key in sorted(set(myblob.tags)):
+            if key[0] == part and len(value) == l:
+                print key,value
+    else:
+        for value,key in sorted(set(myblob.tags)):
+            if key == part:
+                print key,value
+
+def start(chars):
+    for value,key in sorted(myblob.tags):
+        if value.lower().startswith(chars):
+            print key,value
+
+
+def end(chars):
+    for value,key in sorted(myblob.tags):
+        if value.lower().endswith(chars):
+            print key,value
